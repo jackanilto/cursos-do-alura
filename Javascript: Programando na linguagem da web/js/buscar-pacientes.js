8722af1a72902500d8f6dc2b2f9056329b3ab70f
Magno Miranda Dantas <magnomirandadantas@MacBook-Pro-de-Magno.local>
2017-06-26 15:22:13 -0300

    Capítulo 9 do curdo de javascript.

---
+++ Javascript: Programando na linguagem da web/js/buscar-pacientes.js
@@ -0,0 +1,6 @@
+var botaoAdicionar = document.querySelector("#buscar-paciente");
+
+botaoAdicionar.addEventListener("click", function() {
+    console.log("Buscando Pacientes...");
+    // https: //api-pacientes.herokuapp.com/pacientes
+});
