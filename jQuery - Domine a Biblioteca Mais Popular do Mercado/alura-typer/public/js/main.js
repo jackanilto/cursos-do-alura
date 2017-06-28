7d568266f70b10b9d0f41efdb308192f1eda419f
Magno Miranda Dantas <magnomirandadantas@MacBook-Pro-de-Magno.local>
2017-06-28 20:54:37 -0300

    Capitulo 2 Finalizado do curso de jQuery.

---
+++ jQuery - Domine a Biblioteca Mais Popular do Mercado/alura-typer/public/js/main.js
@@ -0,0 +1,17 @@
+var frase = $(".frase").text();
+var numeroPalavras = frase.split(" ").length;
+var tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numeroPalavras);
+
+var campoDigitacao = $(".campo-digitacao");
+campoDigitacao.on("input", function() {
+    var conteudo = campoDigitacao.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+
+
+});
