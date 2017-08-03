862dbc86a72e06a205f7531b8ee223558eb40001
Magno Miranda Dantas <magnomirandadantas@MacBook-Pro-de-Magno.local>
2017-08-03 17:51:53 -0300

    Corrigindo Bug's.

--- jQuery - Domine a Biblioteca Mais Popular do Mercado/alura-typer/public/js/main.js
+++ jQuery - Domine a Biblioteca Mais Popular do Mercado/alura-typer/public/js/main.js
@@ -1,17 +1,53 @@
-var frase = $(".frase").text();
-var numeroPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numeroPalavras);
+var tempoIniciante = $("#tempo-digitacao").text();
+var campo = $(".campo-digitacao");
 
-var campoDigitacao = $(".campo-digitacao");
-campoDigitacao.on("input", function() {
-    var conteudo = campoDigitacao.val();
-
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
-
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
-
-
+$(document).ready(function() {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaJogo();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
+
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numeroPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+
+    tamanhoFrase.text(numeroPalavras);
+}
+
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+
+    });
+}
+
+function inicializaJogo() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+        var cronometro = setInterval(function() {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometro);
+            }
+        }, 1000);
+    });
+}
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-caracteres").text("0");
+    $("#contador-palavras").text("0");
+    $("#tempo-digitacao").text(tempoIniciante);
+    inicializaJogo();
+}
